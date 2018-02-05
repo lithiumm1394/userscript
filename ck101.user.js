f66f6d3b5a51dd19413569834b8ec54470265f35
lithium <lithium1394@gmail.com>
2018-02-05 23:57:42 +0800

    .

--- ck101.user.js
+++ ck101.user.js
@@ -1,29 +1,59 @@
 // ==UserScript==
 // @name ck101
-// @namespace Violentmonkey Scripts
-// @match https://ck101.com/thread-*.html
-// @require https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js
+// @namespace lithium
+// @match https://ck101.com/*
+// @require https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js
 // @grant none
 // ==/UserScript==
 
 $(document).ready(function(){
-	// 删除页眉、页脚和广告
-	$("header.hd").remove();
-	$("div#footer").remove();
-	$("div.mobileAD").remove();
-	$("div.mobileAD_responsive").remove();
-	$("div.mobile_announcements").remove();
-	$("div.another_taste").remove();
-	$("div.extend.mainExtend").remove();
-	$("div#shareArticleWrapper").remove();
+	// 论坛列表
+	// body
+	$(".con_alert_fixed").remove();
+	$("#customPop").remove();
+	$("#fbpic").remove();
+	$("#fb-root").remove();    //cannot delete
 	
-	$("div.viewRate").remove();
-	$("div.box.pd2.mbn.adminBox").remove();
-	$("div.mtn.editor_box").remove();
-	$("a.return-list.addbookmark-icon").remove();
-	$("div#mc_embed_signup").remove();
-	$("div#pageGo").remove();
+	// body > div.all-elements > div.mobile_main
+	$(".sideBtn").siblings().remove();
+	$(".sideBtn").addClass("return-list").unwrap();
+	$("#footer").remove();
 	
-	$("div.wp").css("padding-top", "0");
-	$("div.bottomarea").css("padding-top", "30px");
+	// body > div.all-elements > div.mobile_main > div.wp
+	$(".mobileAD").remove();
+	$(".breadCrumb").remove();
+	$(".mobileAD_responsive").remove();
+	
+	// 论坛列表 stylish
+	$(".wp").css('padding', '0');
+	$(".pg").css('margin-top', '10px');
+	$(".mobile_main").css({'box-shadow':'none'});
+	$(".sideBtn").css({'background-size':'75%', 'right':'12px', 'left':'auto', 'top':'36px'});
+	$(".return-list.toolbox-box-trigger").css({'right':'12px','top':'87px'});
+	$(".goTop.down").css({'right':'12px','top':'138px'});
+	$(".toolbox-box").css({'right':'34.5px','top':'109.5px', 'bottom':'auto', 'transform-origin':'right top 0'});
+
+
+	// 文章页面
+	// div.all-elements
+	$(".return-list.addbookmark-icon").remove();
+
+	// div.all-elements > div.mobile_main > div.wp > div#postlist > div.vt > div.bm
+	$(".mobile_announcements").remove();
+	$(".extend.mainExtend").remove();
+	$("#shareArticleWrapper").remove();
+	$("#mc_embed_signup").remove();
+	$("#pageGo").remove();
+	$(".another_taste").remove();
+	$(".mtn.editor_box").remove();
+
+	// > div.postList > div.pbody
+	$("#pop_ad").remove();
+	// > div.postList > div.pbody > div.mes.hiddenText
+	$(".viewRate").remove();
+	$(".box.pd2.mbn.adminBox").remove();
+	
+	// 文章页面 stylish
+	$(".bottomarea").css("padding-top", "20px");
+	$(".pg").children().css('padding', '5px');
 });
