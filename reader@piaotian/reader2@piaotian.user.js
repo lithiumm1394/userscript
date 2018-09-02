e3eeca7fa66a197a59d559b0adfe5f681d6b785b
lithium <lithium1394@google.com>
2018-09-02 17:10:40 +0800

    .

--- reader@piaotian/reader2@piaotian.user.js
+++ reader@piaotian/reader2@piaotian.user.js
@@ -25,16 +25,14 @@ let retrive = new Promise(
 )
 
 // 建立新html文件
-retrive.then((data) => {
+retrive.then(data => {
   let html = '<html><head>'
   + '<link rel="stylesheet" type="text/css" href="https://rawgit.com/lithiumm1394/userscript/master/reader%40piaotian/stylesheet_new.css">'
   + '<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">'
-  + '<script> var menu = ' + data.menu + '</script>'
   + '</head><body>'
   + '<h1>' + data.title + '</h1>'
   + '<div class="subtitle"><h2>作者：' + data.author + '</h2>'
-  + '<button class="material-icons light-48" onclick="reverseList()">low_priority</button>'
-  + '<script> function reverseList(){ } </script></div>'
+  + '<button class="material-icons light-48">low_priority</button>'
   + '<div class="menu"><ul>'
 
   for (let item of data.menu)
@@ -44,4 +42,8 @@ retrive.then((data) => {
 
   document.write(html)
   document.close()
+
+  return data.menu
+}).then(chapters => {
+  
 })
